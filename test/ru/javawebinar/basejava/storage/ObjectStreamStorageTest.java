d62e52ace172e56e71ff6f3615f2be6562a602f7
Illya Sokolov <illya.sokolov82@gmail.com>
2019-08-05 13:11:09 +0200

    Lesson09 ObjectStreamStorage

---
+++ test/ru/javawebinar/basejava/storage/ObjectStreamStorageTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.basejava.storage;
+
+import static org.junit.Assert.*;
+
+public class ObjectStreamStorageTest extends AbstractStorageTest {
+
+    public ObjectStreamStorageTest() {
+        super(new ObjectStreamStorage(STORAGE_DIR));
+    }
+
+}
